2a3ea3fc36e80e0125b10ad7211196880f00438b
Maria Robles <mariadelamareria@gmail.com>
2019-07-17 19:09:42 +0200

    fixed delete movie from collection name bug

--- src/components/SelectCollection.js
+++ src/components/SelectCollection.js
@@ -36,11 +36,11 @@ class SelectCollection extends React.Component {
               }
               { showingForm &&
                   <form className='select-collection__form' onSubmit={this._handleSubmit}>
-                    <select className='select-collection__select' name='addToCollection' value={this.state.value} onChange={this._handleSelect}>
+                    <select className='select-collection__select' value={this.state.value} onChange={this._handleSelect}>
                       <option className='select-collection__option' value='0'>Chose a collection</option>
                         {
                           collections.map(collection =>
-                            <option className='select-collection__option' value={collection.id} key={collection.id}>
+                            <option className='select-collection__option' data-name={collection.title} value={collection.id} key={collection.id}>
                               {collection.title}
                             </option>
                           )
@@ -58,9 +58,9 @@ class SelectCollection extends React.Component {
                       idCollection={this.state.idCollection}
                       onRating={ratingMovies} 
                     />
-                    <p>This movie belongs to <strong>{this.state.idCollection}</strong> collection.</p>   
+                    <p>This movie belongs to <strong>{this.state.nameCollection}</strong> collection.</p>   
                     <form className='select-collection__form' onSubmit={this._handleSubmitDelete}>
-                      <button className='delete__button' type='submit' >Delete from {this.state.idCollection} collection</button>
+                      <button className='delete__button' type='submit' >Delete from {this.state.nameCollection} collection</button>
                     </form>
                   </>   
                 }
@@ -85,6 +85,7 @@ class SelectCollection extends React.Component {
   componentDidMount = async () => {
     await this.getIdCollection(this.props.movie, this.props.collections)
     await this.getFavMovie(this.props.movie, this.state.idCollection, this.props.collections)
+    await this.getNameCollection(this.props.movie, this.props.collections)
   }
   showForm = () => {
     this.setState({ showingForm: true })
@@ -99,7 +100,10 @@ class SelectCollection extends React.Component {
     this.setState({ showingDelete: false })
   }
   _handleSelect = (event) => {
-    this.setState({ value: event.target.value })
+    var index = event.target.selectedIndex;
+    var optionElement = event.target.childNodes[index]
+    var nameOption =  optionElement.getAttribute('data-name')
+    this.setState({ value: event.target.value, nameCollection: nameOption })
   }
   _handleSubmit = (event) => {
     event.preventDefault()
@@ -142,6 +146,14 @@ class SelectCollection extends React.Component {
       if (collection.favMovies.find(({ id }) => id === currentMovie.id)) {
         console.log('valor de idCollection: ', collection.id)
         this.setState({ idCollection: collection.id })
+      } 
+    })
+  }
+  getNameCollection =  ( currentMovie, collections ) => {
+    collections.map( collection => {
+      if (collection.favMovies.find(({ id }) => id === currentMovie.id)) {
+        console.log('valor de nameCollection: ', collection.title)
+        this.setState({ nameCollection: collection.title})
       } 
     })
   }
